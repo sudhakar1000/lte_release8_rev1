2841c6cada696f3f59c83658d6584e9fd9f8d557
pankaj patial <panku.patial@gmail.com>
2018-04-29 11:26:15 +0530

    added ccch struct info
    ccch stuct

--- test_1/rrc/src/rrc.cpp
+++ test_1/rrc/src/rrc.cpp
@@ -2,6 +2,9 @@
 #include "../inc/rrc.h"
 #include <stdint.h>
 #include <iostream>
+/*
+  Structure to have commomon control channel IEs
+  */
 ul_ccch_message_type ul_ccch_message;
 
 uint64_t mul(uint64_t n1, uint64_t n2)
@@ -18,5 +21,5 @@ void rrc_connection_request_init()
 {
   ul_ccch_message.rrc_connection_request.establishment_cause = mt_Access;
   ul_ccch_message.rrc_connection_request.initial_ue_identity.randomValue = RANDOM_VLAUE;
-  srb0_without_secutity();
+  //srb0_without_secutity();
 }
